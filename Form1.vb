27ef05d692aca94681e6370e4a861ce7c685d035
tshir <tshir@MSI>
2024-01-18 08:50:55 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim A, B, C As Double
+        A = Val(TextBox1.Text)
+        B = Val(TextBox2.Text)
+        C = (A ^ 2 + B ^ 2) * 0.5
+        TextBox3.Text = C
+
+    End Sub
+End Class
